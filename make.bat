a22a87b731d6c83412d3ce4ccff0b3f4ffbd7d3f
nov1n <robert@carosi.nl>
2016-10-31 15:05:27 +0100

    Add batch script for windows

---
+++ make.bat
@@ -0,0 +1,7 @@
+set PEM_PATH="/home/roberto/.ssh/cc_assignment0.pem"
+set PEM_NAME="cc_assignment0"
+set SEC_GROUP="sg-e26fb89b"
+set AWS_REGION="us-west-2"
+set INST_TYPE="t2.micro"
+set IMG_ID="ami-d732f0b7"
+start master.exe
