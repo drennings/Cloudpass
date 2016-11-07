a63498d89a2102ca280fe275f7d8bd186d230e57
nov1n <robert@carosi.nl>
2016-11-07 12:55:29 +0100

    Fix makefile

--- Master/src/main/manager.go
+++ Master/src/main/manager.go
@@ -68,7 +68,7 @@ func (man *Manager) startWorker(w *Worker) {
 		"sudo apt-get update",
 		"sudo apt-get -y install git",
 		"git clone https://github.com/drennings/Cloudpass",
-		"cd Cloudpass/Worker && chmod +x make.sh && sudo ./make.sh",
+		"cd Cloudpass/Worker && chmod +x make.sh && sudo ./make.sh &",
 	})
 	if err != nil {
 		fmt.Printf("%s: an error occurred: %s\n", w.Id, err)
--- Worker/make.sh
+++ Worker/make.sh
@@ -1,4 +1,3 @@
 sudo apt-get -y install python-pip
 sudo pip install -r ./requirements.txt
-sudo python itertest.py &
-exit 0
+sudo python itertest.py
