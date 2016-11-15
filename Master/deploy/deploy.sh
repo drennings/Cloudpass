dc1e3cefef010758371af8aae96f3189ae7a03ab
nov1n <robert@carosi.nl>
2016-11-15 20:11:00 +0100

    Add deploy script for master

---
+++ Master/deploy/deploy.sh
@@ -0,0 +1,7 @@
+export PEM_PATH="/home/roberto/.ssh/cc_assignment0.pem"
+export PEM_NAME="cc_assignment0"
+export SEC_GROUP="sg-e26fb89b"
+export REGION="us-west-2"
+export INST_TYPE="t2.micro"
+export IMG_ID="ami-d732f0b7"
+./aws-automate
